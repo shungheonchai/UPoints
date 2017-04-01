2d3c2ad512de0bc561c1db2b3c3f5854e56f7f56
Timo <tlung@wustl.edu>
2017-04-01 14:24:41 -0500

    static pages changed and added

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,12 @@ class StaticPagesController < ApplicationController
   skip_before_filter :authenticate_user!
 
   def home
-
   end
+
+  def about
+  end
+
+  def liability
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   resources :requests
   devise_for :users
   root to: "static_pages#home"
+  get 'static_pages/home'
+  get 'static_pages/about'
+  get 'static_pages/liability'
+  get 'static_pages/contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
