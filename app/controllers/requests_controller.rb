c0d1ca94ff23685ac07be27f05d13ec7059d1395
Shungheon Chai <chad2212@gmail.com>
2017-04-07 01:01:15 -0500

    Accepting request fully functional with error messages

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,5 @@
 class RequestsController < ApplicationController
-  before_action :set_request, only: [:show, :edit, :update, :destroy]
+  before_action :set_request, only: [:show, :edit, :update, :destroy, :accept_request]
 
 
   # GET /requests
@@ -55,7 +55,21 @@ class RequestsController < ApplicationController
   end
 
   def accept_request
-    
+    current_id = current_user.id
+    respond_to do |format|
+      if current_id != @request.user_id && @request.update(user_other_id: current_id)
+        format.html { redirect_to requests_path, notice: 'You accepted the request' }
+        format.json { render :show, status: :ok, location: @request }
+      else
+        if current_id == @request.user_id
+          format.html { redirect_to requests_path, notice: 'Cannot accept the request: You can\'t accept your own' }
+          format.json { render json: @request.errors, status: :unprocessable_entity }
+        else
+          format.html { redirect_to requests_path, notice: 'Cannot accept the request: update error. Ask system admin' }
+          format.json { render json: @request.errors, status: :unprocessable_entity }
+        end
+      end
+    end
   end
 
   # DELETE /requests/1
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,7 @@ Rails.application.routes.draw do
   resources :ratings
   resources :requests
   devise_for :users
-  root to: "static_pages#home"
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/liability'
-  get 'static_pages/contact'
+  root to: 'static_pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -16,6 +12,5 @@ Rails.application.routes.draw do
     get 'edit_user_registration', to: 'users/registrations#edit'
   end
 
-  resources :requests
-  resources :ratings
+  match 'accept_request', to: 'requests#accept_request', via: :post
 end
