21610f06c002080045289b2f8e07232984e710e7
Shungheon Chai <chad2212@gmail.com>
2017-04-19 14:59:11 -0500

    Report attributes added to Rating and Report model deleted

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170415194526) do
+ActiveRecord::Schema.define(version: 20170419195255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,9 +21,11 @@ ActiveRecord::Schema.define(version: 20170415194526) do
     t.integer  "communication"
     t.integer  "payment"
     t.integer  "overall"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
     t.string   "request_id"
+    t.integer  "rating_user_id"
+    t.text     "report"
   end
 
   create_table "reports", force: :cascade do |t|
