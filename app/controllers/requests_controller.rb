a9892126fa83a156f2c982dee5cb9bf96d58d9ea
Timo <tlung@wustl.edu>
2017-04-25 00:21:07 -0500

    blue highlight for my requests

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -78,7 +78,7 @@ class RequestsController < ApplicationController
 
   def accept_request
     current_id = current_user.id
-    UserMailer.create_accept_request_acceptor(current_user, @request).deliver
+    UserMailer.accept_request_poster(current_user,@request).deliver_now
     UserMailer.create_accept_request_poster(User.find(@request.user_id), @request).deliver
     respond_to do |format|
       if current_id != @request.user_id && @request.update(user_other_id: current_id)
