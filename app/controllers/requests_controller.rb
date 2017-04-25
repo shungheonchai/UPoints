00ef492bf99f8169a510c7945809ef07e7d9e3f3
Shungheon Chai <chad2212@gmail.com>
2017-04-25 00:48:27 -0500

    UserMailer's fixed

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -79,8 +79,8 @@ class RequestsController < ApplicationController
 
   def accept_request
     current_id = current_user.id
-    UserMailer.create_accept_request_acceptor(current_user, @request).deliver
-    UserMailer.create_accept_request_poster(User.find(@request.user_id), @request).deliver
+    UserMailer.accept_request_poster(current_user,@request).deliver_now
+    UserMailer.accept_request_acceptor(User.find(@request.user_id), @request).deliver_now
     respond_to do |format|
       if current_id != @request.user_id && @request.update(user_other_id: current_id)
         format.html { redirect_to requests_path, notice: 'You accepted the request' }
@@ -99,7 +99,7 @@ class RequestsController < ApplicationController
 
   def cancel_request
     current_id = current_user.id
-    UserMailer .create_cancel_request_poster(User.find(@request.user_id), @request).deliver
+    UserMailer .cancel_request_poster(User.find(@request.user_id), @request).deliver_now
     respond_to do |format|
       if current_id != @request.user_id && @request.update(user_other_id: nil)
         format.html { redirect_to requests_path, notice: 'You canceled the request' }
